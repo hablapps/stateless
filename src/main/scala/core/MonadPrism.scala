fb345af8ddade8f807718471c22e4a87c5bf96bb
Jesús López-González <jesus.lopez@hablapps.com>
2017-04-06 12:54:32 +0200

    Adds composition and new methods for `Prism`

--- src/main/scala/core/MonadPrism.scala
+++ src/main/scala/core/MonadPrism.scala
@@ -1,7 +1,10 @@
 package org.hablapps.phoropter
 package core
 
-import scalaz.{ Monad, MonadState, ~> }
+import scalaz.{ Const, Monad, MonadState, ~> }
+import scalaz.syntax.monad._
+import scalaz.std.list._
+import scalaz.std.option._
 
 trait MonadPrism[P[_], Q[_], A] extends Monad[P] {
 
@@ -10,11 +13,58 @@ trait MonadPrism[P[_], Q[_], A] extends Monad[P] {
 
   /* derived algebra */
 
-  def getOpt: P[Option[A]] = hom(MS.get)
+  def getOption: P[Option[A]] = hom(MS.get)
+
   def set(a: A): P[Unit] = map(hom(MS.put(a)))(_.get) // safe by laws
 
-  def modify(f: A => A): P[Unit] =
-    bind(getOpt)(_.fold(point(()))(a => set(f(a))))
+  def modifyOption(f: A => A): P[Option[Unit]] = hom(MS.modify(f))
+
+  def modify(f: A => A): P[Unit] = map(modifyOption(f))(_ => ())
+
+  def isEmpty: P[Boolean] = map(getOption)(_.isEmpty)
+
+  def nonEmpty: P[Boolean] = map(getOption)(_.nonEmpty)
+
+  def find(p: A => Boolean): P[Boolean] = map(getOption)(_.fold(false)(p))
+
+  def exist(p: A => Boolean): P[Boolean] = map(getOption)(_.fold(false)(p))
+
+  def all(p: A => Boolean): P[Boolean] = map(getOption)(_.fold(true)(p))
+
+  /* composing algebras */
+
+  def composeFold[R[_], B](fl: MonadFold[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(λ[R ~> λ[x => P[List[x]]]] { rx =>
+      map(hom(fl.hom(rx)))(_.toList.join)
+    })(this, fl.MR)
+
+  def composeGetter[R[_], B](gt: MonadGetter[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(λ[R ~> λ[x => P[List[x]]]] { rx =>
+      map(hom(gt.hom(rx)))(_.toList)
+    })(this, gt.MR)
+
+  def composeSetter[R[_], B](st: MonadSetter[Q, R, B]): MonadSetter[P, R, B] =
+    MonadSetter(λ[R ~> λ[x => P[Const[Unit, x]]]] { rx =>
+      map(hom(st.hom(rx)))(_ => Const(()))
+    })(this, st.MS)
+
+  def composeTraversal[R[_], B](tr: MonadTraversal[Q, R, B]): MonadTraversal[P, R, B] =
+    MonadTraversal(λ[R ~> λ[x => P[List[x]]]] { rx =>
+      map(hom(tr.hom(rx)))(_.toList.join)
+    })(this, tr.MS)
+
+  def composeOptional[R[_], B](op: MonadOptional[Q, R, B]): MonadOptional[P, R, B] =
+    MonadOptional(λ[R ~> λ[x => P[Option[x]]]] { rx =>
+      map(hom(op.hom(rx)))(_.join)
+    })(this, op.MS)
+
+  def composePrism[R[_], B](pr: MonadPrism[Q, R, B]): MonadPrism[P, R, B] =
+    MonadPrism(λ[R ~> λ[x => P[Option[x]]]] { rx =>
+      map(hom(pr.hom(rx)))(_.join)
+    })(this, pr.MS)
+
+  def composeLens[R[_], B](ln: MonadLens[Q, R, B]): MonadOptional[P, R, B] =
+    MonadOptional(hom compose ln.hom)(this, ln.MS)
 }
 
 object MonadPrism {
