0077166303acbe1120bbf6aaea9d6c8e3128e529
Jesús López-González <jesus.lopez@hablapps.com>
2017-04-06 13:08:25 +0200

    Adds composition for `Setter`

--- src/main/scala/core/MonadSetter.scala
+++ src/main/scala/core/MonadSetter.scala
@@ -2,6 +2,8 @@ package org.hablapps.phoropter
 package core
 
 import scalaz.{ Const, Monad, MonadState, ~> }
+import scalaz.syntax.monad._
+import scalaz.std.option._
 
 trait MonadSetter[P[_], Q[_], A] extends Monad[P] {
 
@@ -13,6 +15,33 @@ trait MonadSetter[P[_], Q[_], A] extends Monad[P] {
   def modify(f: A => A): P[Unit] = map(hom(MS.modify(f)))(_.getConst)
 
   def set(a: A): P[Unit] = modify(_ => a)
+
+  /* composing algebras */
+
+  def composeSetter[R[_], B](st: MonadSetter[Q, R, B]): MonadSetter[P, R, B] =
+    MonadSetter(λ[R ~> λ[x => P[Const[Unit, x]]]] { rx =>
+      map(hom(st.hom(rx)))(_ => Const(()))
+    })(this, st.MS)
+
+  def composeTraversal[R[_], B](tr: MonadTraversal[Q, R, B]): MonadSetter[P, R, B] =
+    MonadSetter(λ[R ~> λ[x => P[Const[Unit, x]]]] { rx =>
+      map(hom(tr.hom(rx)))(_ => Const(()))
+    })(this, tr.MS)
+
+  def composeOptional[R[_], B](op: MonadOptional[Q, R, B]): MonadSetter[P, R, B] =
+    MonadSetter(λ[R ~> λ[x => P[Const[Unit, x]]]] { rx =>
+      map(hom(op.hom(rx)))(_ => Const(()))
+    })(this, op.MS)
+
+  def composePrism[R[_], B](pr: MonadPrism[Q, R, B]): MonadSetter[P, R, B] =
+    MonadSetter(λ[R ~> λ[x => P[Const[Unit, x]]]] { rx =>
+      map(hom(pr.hom(rx)))(_ => Const(()))
+    })(this, pr.MS)
+
+  def composeLens[R[_], B](ln: MonadLens[Q, R, B]): MonadSetter[P, R, B] =
+    MonadSetter(λ[R ~> λ[x => P[Const[Unit, x]]]] { rx =>
+      map(hom(ln.hom(rx)))(_ => Const(()))
+    })(this, ln.MS)
 }
 
 object MonadSetter {
