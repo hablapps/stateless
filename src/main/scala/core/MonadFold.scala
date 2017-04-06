f45150ae90131d29d570b3393743f895655bcb5c
Jesús López-González <jesus.lopez@hablapps.com>
2017-04-06 10:42:18 +0200

    Adds composition and new methods for `Fold`

--- src/main/scala/core/MonadFold.scala
+++ src/main/scala/core/MonadFold.scala
@@ -1,7 +1,10 @@
 package org.hablapps.phoropter
 package core
 
-import scalaz.{ Monad, MonadReader, ~> }
+import scalaz.{ Monad, MonadReader, Monoid, ~> }
+import scalaz.syntax.foldable._
+import scalaz.syntax.monad._
+import scalaz.std.list._
 
 trait MonadFold[P[_], Q[_], A] extends Monad[P] {
 
@@ -11,6 +14,48 @@ trait MonadFold[P[_], Q[_], A] extends Monad[P] {
   /* derived algebra */
 
   def getAll: P[List[A]] = hom(MR.ask)
+
+  def foldMap[M: Monoid](f: A => M): P[M] = map(getAll)(_.foldMap(f))
+
+  def find(p: A => Boolean): P[Option[A]] = map(getAll)(_.find(p))
+
+  def headOption: P[Option[A]] = map(getAll)(_.headOption)
+
+  def lastOption: P[Option[A]] = map(getAll)(_.lastOption)
+
+  def exist(p: A => Boolean): P[Boolean] = map(getAll)(_.exists(p))
+
+  def all(p: A => Boolean): P[Boolean] = map(getAll)(_.all(p))
+
+  def length: P[Int] = map(getAll)(_.length)
+
+  def isEmpty: P[Boolean] = map(getAll)(_.isEmpty)
+
+  def nonEmpty: P[Boolean] = map(isEmpty)(! _)
+
+  /* composing algebras */
+
+  def composeFold[R[_], B](fl: MonadFold[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(λ[R ~> λ[x => P[List[x]]]](rx => map(hom(fl.hom(rx)))(_.join)))(this, fl.MR)
+
+  def composeGetter[R[_], B](gt: MonadGetter[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(hom compose gt.hom)(this, gt.MR)
+
+  def composeTraversal[R[_], B](tr: MonadTraversal[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(λ[R ~> λ[x => P[List[x]]]](rx => map(hom(tr.hom(rx)))(_.join)))(this, tr.MS)
+
+  def composeOptional[R[_], B](op: MonadOptional[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(λ[R ~> λ[x => P[List[x]]]] { rx =>
+      map(hom(op.map(op.hom(rx))(_.toList)))(_.join)
+    })(this, op.MS)
+
+  def composePrism[R[_], B](pr: MonadOptional[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(λ[R ~> λ[x => P[List[x]]]] { rx =>
+      map(hom(pr.map(pr.hom(rx))(_.toList)))(_.join)
+    })(this, pr.MS)
+
+  def composeLens[R[_], B](ln: MonadLens[Q, R, B]): MonadFold[P, R, B] =
+    MonadFold(hom compose ln.hom)(this, ln.MS)
 }
 
 object MonadFold {
---
+++ src/main/scala/core/package.scala
@@ -0,0 +1,15 @@
+package org.hablapps.phoropter
+
+import scalaz.{ MonadState, MonadReader }
+
+package object `core` {
+
+  // XXX: this has to be standard somewhere
+  implicit def fromStateToReader[F[_], A](ms: MonadState[F, A]): MonadReader[F, A] =
+    new MonadReader[F, A] {
+      def point[X](x: => X) = ms.point(x)
+      def bind[X, Y](fx: F[X])(f: X => F[Y]): F[Y] = ms.bind(fx)(f)
+      def ask = ms.get
+      def local[X](f: A => A)(fx: F[X]): F[X] = ms.bind(ms.modify(f))(_ => fx)
+    }
+}
