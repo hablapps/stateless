9312f6fde716b05eb30afe9d6379a94411186360
Jesús López-González <jesus.lopez@hablapps.com>
2017-04-11 10:11:46 +0200

    Adds remaining asymmetric indexed optics

--- src/main/scala/core/asymmetric/nat/indexed/IGetterAlg.scala
+++ src/main/scala/core/asymmetric/nat/indexed/IGetterAlg.scala
@@ -8,7 +8,8 @@ import scalaz.{ Monad, MonadReader, ~> }
 import scalaz.Id.Id
 import scalaz.syntax.functor._
 
-trait IGetterAlg[P[_], Q[_], I, A] extends IOpticAlg[P, Q, I, A, MonadReader, Id] {
+trait IGetterAlg[P[_], Q[_], I, A] extends raw.indexed.IGetterAlg[P, I, A]
+    with IOpticAlg[P, Q, I, A, MonadReader, Id] {
 
   def get: P[(I, A)] = hom(ev.ask.strengthL)
 }
