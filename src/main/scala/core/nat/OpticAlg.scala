558c2f0177c6178e7e1de59d729399ad7dbf7997
Jesús López-González <jesus.lopez@hablapps.com>
2017-06-22 17:21:29 +0200

    Contemplates Haskell in gitignore

--- src/main/scala/core/nat/OpticAlg.scala
+++ src/main/scala/core/nat/OpticAlg.scala
@@ -2,16 +2,14 @@ package org.hablapps.stateless
 package core
 package nat
 
-import scalaz.{ Equal, Functor, Monad, ~> }
-import scalaz.syntax.equal._
-import scalaz.syntax.functor._
+import scalaz.{ Functor, Monad, ~> }
 
-trait OpticAlg[P[_], A, Ev[M[_], _] <: Monad[M], F[_]] extends Monad[P] { self =>
+trait OpticAlg[P[_], A, Ev[M[_], _] <: Monad[M], F[_]] extends Monad[P] {
 
   type Q[_]
 
   implicit val ev: Ev[Q, A]
-  
+
   implicit val fev: Functor[F]
 
   val hom: Q ~> λ[x => P[F[x]]]
