1c5d86f7b956e945b9eee7796384f3f631a0a6fe
Jesús López-González <jesus.lopez@hablapps.com>
2017-06-30 11:31:35 +0200

    Natural transformation (no typeclass) hs version

--- doc/stateless.hs
+++ doc/stateless.hs
@@ -1,35 +1,46 @@
-{-# LANGUAGE FlexibleContexts
-  , FlexibleInstances
-  , MultiParamTypeClasses
-  , RankNTypes
-  , TypeFamilies
-  , TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Stateless where
 
 import Control.Monad.Identity
+import Control.Monad.Reader
 import Control.Monad.State
-import Control.Monad.State.Class
-import Data.Functor
+import Data.Functor.Constant
+import Data.HList.HList
+import Data.Map
+import Data.Monoid
+import GHC.TypeLits
 
 -- -------------------------
 -- -- Optics as Machines! --
 -- -------------------------
---
--- -- data Iso s a = Iso { runIso :: s -> (a, a -> s) }
--- newtype Lens s a = Lens { runLens :: s -> (a, a -> s) }
---
--- -- newtype Prism s a = Prism { runPrism :: s -> (Maybe a, a -> s) }
--- newtype Affine s a = Affine { runAffine :: s -> (Maybe a, a -> s) }
---
--- newtype Getter s a = Getter { runGetter :: s -> a }
---
--- newtype Fold s a = Fold { runFold :: s -> [a] }
---
--- newtype Setter s a = Setter { runSetter :: s -> (a -> a) -> s }
---
--- -- for a fixed size N
--- newtype Traversal s a = Traversal { runTraversal :: s -> ([a], [a] -> s) }
+
+-- data Iso s a = Iso { runIso :: s -> (a, a -> s) }
+newtype Lens s a = Lens { runLens :: s -> (a, a -> s) }
+
+-- newtype Prism s a = Prism { runPrism :: s -> (Maybe a, a -> s) }
+newtype Affine s a = Affine { runAffine :: s -> (Maybe a, a -> s) }
+
+newtype Getter s a = Getter { runGetter :: s -> a }
+
+newtype Fold s a = Fold { runFold :: s -> [a] }
+
+newtype Setter s a = Setter { runSetter :: s -> (a -> a) -> s }
+
+-- for a fixed size N
+newtype Traversal s a = Traversal { runTraversal :: s -> ([a], [a] -> s) }
 --
 -- --------------------------------
 -- -- Classic Optics (with laws) --
@@ -72,54 +83,276 @@ import Data.Functor
 -- Optic Algebras --
 --------------------
 
--- Raw representation
+-- Natural Representation (Experimental, No Typeclasses)
 
--- MonadState
--- class LensAlg p a where
---   la_get :: p a
---   la_put :: a -> p ()
+-- XXX: what are the implications of type constraints?
 
--- Natural representation
+type OpticAlg p q f = forall x. q x -> p (f x)
 
-class (Monad p, MonadState a (Q a)) => LensAlg p a where
-  data Q a :: * -> *
-  hom :: Q a ~> p
+type LensAlg p q a = (Monad p, MonadState a q) => OpticAlg p q Identity
 
-  get' :: p a
-  get' = hom get
-  put' :: a -> p ()
-  put' a = hom (put a)
-  mod' :: (a -> a) -> p ()
-  mod' f = get' >>= (put' . f)
+type GetterAlg p q a = (Monad p, MonadReader a q) => OpticAlg p q Identity
 
-instance Functor (Q Int) where
-  fmap f (Wrapper s) = Wrapper (fmap f s)
+type TraversalAlg p q a = (Monad p, MonadState a q) => OpticAlg p q []
 
-instance Applicative (Q Int) where
-  pure = Wrapper . pure
-  (Wrapper s) <*> (Wrapper f) = Wrapper $ s <*> f
+type FoldAlg p q a = (Monad p, MonadReader a q) => OpticAlg p q []
 
-instance Monad (Q Int) where
-  return = Wrapper . return
-  (Wrapper s) >>= f = Wrapper $ s >>= (runWrapper . f)
+type AffineAlg p q a = (Monad p, MonadState a q) => OpticAlg p q Maybe
 
-instance MonadState Int (Q Int) where
-  get = Wrapper get
-  put a = Wrapper (put a)
+type SetterAlg p q a = (Monad p, MonadState a q) => OpticAlg p q (Constant ())
 
-data Person = Person { name :: String, age :: Int } deriving Show
+-- indexed
 
-instance LensAlg (State Person) Int where
-  data Q Int b = Wrapper { runWrapper :: State Int b }
-  hom qix = StateT (\p -> Identity (
-    runIdentity $ evalStateT (runWrapper qix) (age p),
-    p { age = runIdentity $ execStateT (runWrapper qix) (age p) }))
+type IOpticAlg i p q f = forall x. (i -> q x) -> p (f x)
 
-getAge :: Person -> Int
-getAge p = runIdentity $ evalStateT get' p
+type ITraversalAlg i p q a = (Monad p, MonadState a q) => IOpticAlg i p q []
 
-putAge :: Person -> Int -> Person
-putAge p a = runIdentity $ execStateT (put' a) p
+-- composition
+
+compose :: (Functor p) =>
+           (forall x. f (g x) -> f x) ->
+           OpticAlg p q f ->
+           OpticAlg q r g ->
+           OpticAlg p r f
+compose j o1 o2 = fmap j . o1 . o2
+
+composeM :: (Functor p, Monad m) =>
+            OpticAlg p q m ->
+            OpticAlg q r m ->
+            OpticAlg p r m
+composeM = compose join
+
+(^|->) :: (Monad p, Monad q, MonadState a q, MonadState b r) =>
+          TraversalAlg p q a ->
+          LensAlg q r b ->
+          TraversalAlg p r b
+(^|->) = compose (fmap runIdentity)
+
+asIndexed :: OpticAlg p q f -> IOpticAlg (HList '[]) p q f
+asIndexed op f = op $ f HNil
+
+fromIndexed :: IOpticAlg (HList '[]) p q f -> OpticAlg p q f
+fromIndexed iop qx = iop (const qx)
+
+-- indexed composition
+
+icompose :: (Functor p, HAppendList l1 l2) =>
+            (forall x. f (g x) -> f x) ->
+            IOpticAlg (HList l1) p q f ->
+            IOpticAlg (HList l2) q r g ->
+            IOpticAlg (HList (HAppendListR l1 l2)) p r f
+icompose j iop1 iop2 f = j <$> iop1 (\i -> iop2 (f . hAppendList i))
+
+-- XXX: `HAppendListR l '[]` is not returning `l`!
+(^^|->) :: (Monad p, Monad q, MonadState a q, MonadState b r, HAppendList l '[]) =>
+           ITraversalAlg (HList l) p q a ->
+           LensAlg q r b ->
+           ITraversalAlg (HList (HAppendListR l '[])) p r b
+(^^|->) itr ln = icompose (fmap runIdentity) itr (asIndexed ln)
+
+-- functions
+
+view :: (MonadState a q) => OpticAlg p q f -> p (f a)
+view op = op get
+
+set :: (MonadState a q) => OpticAlg p q f -> a -> p (f ())
+set op a = op $ put a
+
+modi :: (MonadState a q) => OpticAlg p q f -> (a -> a) -> p (f ())
+modi op f = op $ modify f
+
+fold :: (Functor p, MonadState a q, Foldable f, Monoid m) =>
+        OpticAlg p q f -> (a -> m) -> p m
+fold op f = fmap (foldMap f) (view op)
+
+-- indexed functions
+
+-- XXX: where's strength? (category-extras is obsolete!?!?)
+iview :: (MonadState a q) => IOpticAlg i p q f -> p (f (i, a))
+iview op = op (\i -> fmap ((,) i) get)
+
+iindex :: (Functor p, Functor f, MonadState a q) => IOpticAlg i p q f -> p (f i)
+iindex = fmap (fmap fst) . iview
+
+ifold :: (Functor p, MonadState a q, Foldable f, Monoid m) =>
+         IOpticAlg i p q f -> ((i, a) -> m) -> p m
+ifold iop f = fmap (foldMap f) (iview iop)
+
+imodi :: (MonadState a q) => IOpticAlg i p q f -> (a -> a) -> p (f ())
+imodi iop f = iop (const $ modify f)
+
+-- university
+
+class UniversityView p where
+  universityName :: p String
+  departmentNames :: p [String]
+  totalBudget :: p Int
+  duplicateBudget :: p ()
+
+class UniversityData p q d | p -> q, q -> p, q -> d where
+  name :: LensAlg p (State String) String
+  departments :: ITraversalAlg (HList '[String]) p q d
+  budget :: LensAlg q (State Int) Int
+
+-- XXX: requires undecidable and ambiguous
+instance (Monad p, Monad q, MonadState d q, UniversityData p q d) => UniversityView p where
+  universityName = runIdentity <$> view name
+  departmentNames = fmap hHead <$> iindex departments
+  totalBudget = getSum <$> ifold (departments ^^|-> budget) (Sum . snd)
+  duplicateBudget = void $ imodi (departments ^^|-> budget) (* 2)
+
+-- instantiating university
+
+data University = University { nm :: String, deps :: Map String Department } deriving Show
+newtype Department = Department { bud :: Int } deriving Show
+
+instance UniversityData (State University) (State Department) Department where
+  name sn = StateT (\u -> let (a, nm2) = runState sn (nm u) in
+    Identity (Identity a, u { nm = nm2 }))
+  departments sd =
+    StateT (\u ->
+      let (as, deps2) = unzip $ fmap (\(k, d) -> fmap ((,) k) (runState (sd (k `HCons` HNil)) d)) (assocs $ deps u) in
+        Identity (as, u { deps = fromList deps2 }))
+  budget sb = StateT (\d -> let (a, bud2) = runState sb (bud d) in
+    Identity (Identity a, d { bud = bud2 }))
+
+main :: IO ()
+main = do
+  putStrLn "URJC budget: "
+  let uni = University "URJC" (fromList [("CS", Department 1000), ("HS", Department 2000)])
+  putStr "* Total budget: "
+  print $ evalState totalBudget uni
+  putStr "* After duplicating budget: "
+  print $ runState duplicateBudget uni
+  putStr "* Department names: "
+  print $ evalState departmentNames uni
+
+-- End of experimental
+
+-- class (Monad p, MonadState (Aln q) q) => LensAlg p q where
+--   type Aln q
+--   lnHom :: q ~> p
+--
+-- class (Monad p, MonadState (Atr q) q) => TraversalAlg p q where
+--   type Atr q
+--   trHom :: q x -> p [x]
+--
+-- instance (LensAlg p q, LensAlg q r) => LensAlg p r where
+--   type Aln r
+--   lnHom = undefined
+
+-- -- Natural representation
+--
+-- -- XXX: `p -> q` is necessary to get things working in Haskell, but we don't
+-- -- want stateless to behave like that. In fact, we want heterogeneity!
+--
+-- class (Monad p, MonadState a q) => NatLensAlg p q a where
+--   lnHom :: q ~> p
+--
+-- class (Monad p, MonadState a q) => NatTraversalAlg p q a where
+--   trHom :: forall x. q x -> p [x]
+--
+--   trGet :: p [a]
+--   trGet = trHom get
+--   trFoldMap :: (Monoid m) => (a -> m) -> p m
+--   trFoldMap f = fmap (foldMap f) trGet
+--
+-- class (Monad p, MonadReader a q) => NatGetterAlg p q a | p -> q where
+--   gtHom :: q ~> p
+--
+-- class (Monad p, MonadReader a q) => NatFoldAlg p q a | p -> q where
+--   flHom :: forall x. q x -> p [x]
+--
+-- class (Monad p, MonadState a q) => NatAffineAlg p q a | p -> q where
+--   afHom :: forall x. q x -> p (Maybe x)
+--
+-- class (Monad p, MonadState a q) => NatSetterAlg p q a | p -> q where
+--   stHom :: forall x. q x -> p (Constant () x)
+--
+-- ------------------
+-- -- Composition ---
+-- ------------------
+--
+-- -- XXX: we are limited in the number of instances we can create, this is
+-- -- Haskell! Therefore we need a bunch of newtypes to make instances distinguible
+-- -- from each other
+--
+-- newtype LL a = LL { getLL :: a }
+-- newtype LG a = LG { getLG :: a }
+-- newtype LS a = LS { getLS :: a }
+-- newtype TL a = TL { getTL :: a }
+-- newtype LT a = LT { getLT :: a }
+--
+-- -- XXX: this is a nonsense. If `p -> q` is a dependency, then `p` determines a
+-- -- unique `q`. Then, we say that there's a valid instance for `p` and `r`, so
+-- -- `p -> r`.
+-- instance (NatLensAlg p q a, NatLensAlg q r (LL b)) => NatLensAlg p r (LL b) where
+--   lnHom = lnHom . lnHom
+--
+-- instance (NatLensAlg p q a, NatTraversalAlg q r (LT b)) => NatTraversalAlg p r (LT b) where
+--   trHom = lnHom . trHom
+--
+-- instance (NatTraversalAlg p q a, NatLensAlg q r (TL b)) => NatTraversalAlg p r (TL b) where
+--   trHom = trHom . lnHom
+--
+-- instance (NatLensAlg p q a, NatGetterAlg q r (LG b)) => NatGetterAlg p r (LG b) where
+--   gtHom = lnHom . gtHom
+--
+-- instance (NatLensAlg p q a, NatSetterAlg q r (LS b)) => NatSetterAlg p r (LS b) where
+--   stHom = lnHom . stHom
+--
+-- ----------------
+-- -- Nat 2 Raw ---
+-- ----------------
+--
+-- instance (NatLensAlg p q a) => RawLensAlg p a where
+--   la_get = lnHom get
+--   la_put a = lnHom (put a)
+--
+-- instance (NatTraversalAlg p q a) => RawTraversalAlg p a where
+--   tr_get = trHom get
+--   tr_modify f = void $ trHom (modify f)
+--
+-- -- XXX: What is "UndecidableInstances" extension? I need it to keep instances
+-- -- happy!
+--
+-- -----------------
+-- -- University ---
+-- -----------------
+--
+-- -- view
+--
+-- type Name = String
+-- type Budget = Int
+--
+-- class University p d where
+--   name :: p Name
+--   removeDepartment :: Name -> p ()
+--   addDepartment :: Name -> Int -> [(String, String, Int)] -> p ()
+--   updateSalary :: (Int -> Int) -> p ()
+--   upperCase :: p ()
+--   containsDepartment :: String -> p Bool
+--   getTotalSalary :: p Int
+--   getNameInitials :: p String
+--   getTotalBudget :: p Budget
+--   getBudget :: String -> p (Maybe Budget)
+--
+-- -- data
+--
+-- instance (NatLensAlg p (State Name) Name,
+--           NatTraversalAlg p q d,
+--           NatLensAlg q (State (TL Budget)) (TL Budget)) => University p d where
+--   name = la_get
+--   getTotalBudget =
+--     -- ERROR: fmap getSum (tr_foldMap (Sum . getTL))
+--     -- fmap getSum (trFoldMap (Sum . getTL))
+--     fmap (getSum . foldMap (Sum . getTL)) (trHom (get :: State (TL Budget) (TL Budget)))
+--
+-- --------------------------
+-- -- University Instance ---
+-- --------------------------
+--
+-- -- TODO: State, other effects? bbdd libraries...
 
 --------------
 -- Sandbox ---
@@ -152,10 +385,6 @@ putAge p a = runIdentity $ execStateT (put' a) p
 
 type (~>) f g = forall a . f a -> g a
 
-maybeToList :: Maybe ~> []
-maybeToList (Just a) = [a]
-maybeToList Nothing = []
-
 -- Type families
 
 data family MyFamily a :: *
@@ -168,3 +397,99 @@ familyFun Jesus = True
 familyFun' :: MyFamily Char -> Bool
 familyFun' (Godfather _ _) = False
 familyFun' (Father _) = False
+
+type family And (a :: Bool) (b :: Bool) :: Bool where
+  And 'False c      = 'False
+  And 'True  d      = d
+  And e      'False = 'False
+  And f      'True  = f
+  And g      g      = g
+
+data Vector :: Nat -> * -> * where
+  VNil :: Vector 0 a
+  (:*:) :: a -> Vector n a -> Vector (n + 1) a
+
+infixr 5 :*:
+
+unsafeHead :: Vector n a -> a
+unsafeHead VNil = undefined
+unsafeHead (a :*: _) = a
+
+type family NonZero (a :: Nat) :: Bool where
+  NonZero 0 = 'False
+  NonZero x = 'True
+
+safeHead :: NonZero n ~ 'True => Vector n a -> a
+safeHead (a :*: _) = a
+
+v :: Vector 3 Int
+v = 1 :*: 2 :*: 3 :*: VNil
+
+x :: Int
+x = safeHead v
+
+newtype I f = I f
+newtype C f = C f
+data S f = S String f
+
+formatSpec :: S (I (S (C String)))
+formatSpec = S "Int: " $ I $ S ", Char: " $ C "."
+
+class Format fmt where
+  type Sprintf fmt
+  sprintf' :: String -> fmt -> Sprintf fmt
+
+instance Format String where
+  type Sprintf String = String
+  sprintf' pre str = pre ++ str
+
+instance Format a => Format (I a) where
+  type Sprintf (I a) = Int -> Sprintf a
+  sprintf' pre (I a) i = sprintf' (pre ++ show i) a
+
+instance Format a => Format (C a) where
+  type Sprintf (C a) = Char -> Sprintf a
+  sprintf' pre (C a) c = sprintf' (pre ++ show c) a
+
+instance Format a => Format (S a) where
+  type Sprintf (S a) = Sprintf a
+  sprintf' pre (S s a) = sprintf' (pre ++ s) a
+
+sprintf :: Format fmt => fmt -> Sprintf fmt
+sprintf = sprintf' ""
+
+xyz :: Int -> Char -> String
+xyz = sprintf formatSpec
+
+-- Functional dependencies paper
+
+class Collects ce e | ce -> e where
+  empty  :: ce
+  insert :: e -> ce -> ce
+  member :: e -> ce -> Bool
+  def    :: e
+
+  -- -- This can't be done, since `def` can't uniquely determine `ce`
+  -- insertDef :: ce -> ce
+  -- insertDef = insert def
+
+class Collects2 ce where
+  type E ce
+  empty2  :: ce
+  insert2 :: E ce -> ce -> ce
+  member2 :: E ce -> ce -> Bool
+  def2    :: E ce
+
+  -- -- Same here, `E ce` can't determine `ce` by its own.
+  -- insertDef2 :: ce -> ce
+  -- insertDef2 = insert2 def2
+
+class Monoide m where
+  mappend :: m -> m -> m
+  mzero :: m
+
+instance Monoide Int where
+  mappend = (+)
+  mzero = 0
+
+newtype Int' = Go Int deriving Monoide
