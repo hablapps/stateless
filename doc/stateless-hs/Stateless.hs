c336c3c8c78f25de830baad21a905e55673ddb68
Jesús López-González <jesus.lopez@hablapps.com>
2017-07-05 10:03:43 +0200

    Adds `addDepartment` logic, using `At` operation

--- doc/stateless-hs/Stateless.hs
+++ doc/stateless-hs/Stateless.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Stateless where
@@ -9,62 +11,7 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Data.Functor.Constant
 import Data.HList.HList
-
--- -------------------------
--- -- Optics as Machines! --
--- -------------------------
-
--- data Iso s a = Iso { runIso :: s -> (a, a -> s) }
-newtype Lens s a = Lens { runLens :: s -> (a, a -> s) }
-
--- newtype Prism s a = Prism { runPrism :: s -> (Maybe a, a -> s) }
-newtype Affine s a = Affine { runAffine :: s -> (Maybe a, a -> s) }
-
-newtype Getter s a = Getter { runGetter :: s -> a }
-
-newtype Fold s a = Fold { runFold :: s -> [a] }
-
-newtype Setter s a = Setter { runSetter :: s -> (a -> a) -> s }
-
--- for a fixed size N
-newtype Traversal s a = Traversal { runTraversal :: s -> ([a], [a] -> s) }
-
--- --------------------------------
--- -- Classic Optics (with laws) --
--- --------------------------------
---
--- data Lens' s a = Lens' { get :: s -> a, put :: a -> s -> s }
---
--- prop_PutGet l s a     = get l (put l s a) == a
--- prop_GetPut l s       = put l (get s) s   == s
--- prop_PutPut l s a1 a2 = (put l a1 . put l a2) s == put l a2 s
---
--- data Affine' s a = Affine' { getMaybe :: s -> Maybe a
---                            , put' :: a -> s -> s }
---
--- prop_PutGetm af s a      = getMaybe af (put' af a s) == (getMaybe af s $> a)
--- prop_GetPutm af s        = fmap (\a -> put' af a s) (getMaybe af s) == getMaybe af s
--- prop_PutmPutm af s a1 a2 = (put' af a1 . put' af a2) s == put' af a1 s
---
--- newtype Getter' s a = Getter' { get' :: s -> a }
---
--- -- no Getter laws
---
--- newtype Fold' s a = Fold' { getList :: s -> [a] }
---
--- -- no Fold laws
---
--- newtype Setter' s a = Setter' { modify :: (a -> a) -> s -> s }
---
--- prop_ModId  st     s = modify st id s == s
--- prop_ModCom st f g s = (modify st f . modify st g) s == modify st (f . g) s
---
--- data Traversal' s a = Traversal' { getList' :: s -> [a]
---                                  , putList :: [a] -> s -> s }
---
--- prop_PutlGetl t s a     = getList' t (putList t s a) == a
--- prop_GetlPutl t s       = putList t (getList' t s) s == s
--- prop_PutlPutl t s as1 as2 = (putList t as1 . putList t as2) s == putList t as2 s
+import Data.Monoid
 
 --------------------
 -- Optic Algebras --
@@ -174,11 +121,23 @@ ifold :: (Monad p, MonadState a q, Functor f, Foldable f, Monoid m) =>
          IOpticAlg i p q f -> ((i, a) -> m) -> p m
 ifold iop f = fmap (foldMap f) (iview iop)
 
+icontains :: (Monad p, MonadState a q, Functor f, Foldable f, Eq i) =>
+             IOpticAlg i p q f -> i -> p Bool
+icontains iop i = getAny <$> ifold iop (\(i2, _) -> Any $ i == i2)
+
 imodi :: (Monad p, MonadState a q, Functor f) =>
          IOpticAlg i p q f -> (i -> a -> a) -> p (f ())
 imodi iop f = iop (modify . f)
 
--- weird composition
+ifilter :: (Monad p, MonadState a q, Eq i, FilterIndex i p q a) =>
+           (i -> Bool) -> q x -> p [x]
+ifilter p qx = filterIndex p (const qx)
+
+ipick :: (Monad p, MonadState a q, Eq i, FilterIndex i p q a) =>
+         i -> q x -> p [x]
+ipick i = ifilter (== i)
+
+-- exotic composition
 
 parLens :: LensAlg p q a -> LensAlg p q a -> TraversalAlg p q a
 parLens l1 l2 q = (\(Identity a1) (Identity a2) -> [a1, a2]) <$> l1 q <*> l2 q
@@ -189,3 +148,68 @@ xcompose :: (Monad q, Functor g) =>
             OpticAlg q r g ->
             IOpticAlg (HList l) p r f
 xcompose j iop op rx = j <$> iop (op . rx)
+
+-- Ops
+
+class At i p q a | p -> q, q -> a where
+  at :: i -> LensAlg p q (Maybe a)
+
+class FilterIndex i p q a | p -> q, q -> a where
+  filterIndex :: (i -> Bool) -> ITraversalAlg i p q a
+
+
+-------------------------
+-- Optics as Machines! --
+-------------------------
+
+-- data Iso s a = Iso { runIso :: s -> (a, a -> s) }
+newtype Lens s a = Lens { runLens :: s -> (a, a -> s) }
+
+-- newtype Prism s a = Prism { runPrism :: s -> (Maybe a, a -> s) }
+newtype Affine s a = Affine { runAffine :: s -> (Maybe a, a -> s) }
+
+newtype Getter s a = Getter { runGetter :: s -> a }
+
+newtype Fold s a = Fold { runFold :: s -> [a] }
+
+newtype Setter s a = Setter { runSetter :: s -> (a -> a) -> s }
+
+-- for a fixed size N
+newtype Traversal s a = Traversal { runTraversal :: s -> ([a], [a] -> s) }
+
+-- --------------------------------
+-- -- Classic Optics (with laws) --
+-- --------------------------------
+--
+-- data Lens' s a = Lens' { get :: s -> a, put :: a -> s -> s }
+--
+-- prop_PutGet l s a     = get l (put l s a) == a
+-- prop_GetPut l s       = put l (get s) s   == s
+-- prop_PutPut l s a1 a2 = (put l a1 . put l a2) s == put l a2 s
+--
+-- data Affine' s a = Affine' { getMaybe :: s -> Maybe a
+--                            , put' :: a -> s -> s }
+--
+-- prop_PutGetm af s a      = getMaybe af (put' af a s) == (getMaybe af s $> a)
+-- prop_GetPutm af s        = fmap (\a -> put' af a s) (getMaybe af s) == getMaybe af s
+-- prop_PutmPutm af s a1 a2 = (put' af a1 . put' af a2) s == put' af a1 s
+--
+-- newtype Getter' s a = Getter' { get' :: s -> a }
+--
+-- -- no Getter laws
+--
+-- newtype Fold' s a = Fold' { getList :: s -> [a] }
+--
+-- -- no Fold laws
+--
+-- newtype Setter' s a = Setter' { modify :: (a -> a) -> s -> s }
+--
+-- prop_ModId  st     s = modify st id s == s
+-- prop_ModCom st f g s = (modify st f . modify st g) s == modify st (f . g) s
+--
+-- data Traversal' s a = Traversal' { getList' :: s -> [a]
+--                                  , putList :: [a] -> s -> s }
+--
+-- prop_PutlGetl t s a     = getList' t (putList t s a) == a
+-- prop_GetlPutl t s       = putList t (getList' t s) s == s
+-- prop_PutlPutl t s as1 as2 = (putList t as1 . putList t as2) s == putList t as2 s
