8914e53e14a228743b0759a156efa1dc3533f86f
Jesús López-González <jesus@kathars.es>
2017-08-06 21:59:33 +0200

    sync

--- doc/stateless-hs/Geofences.hs
+++ doc/stateless-hs/Geofences.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Geofences where
@@ -10,6 +12,9 @@ import Control.Monad.State
 
 import Data.Map as Map
 import Data.Set as Set
+
+import Database.HDBC
+import Database.HDBC.Sqlite3
 
 import Stateless
 
@@ -120,9 +125,9 @@ fromDL dl nvf = case nvf of
     traverse (\i -> set (runAt' (geofence dl) i) Nothing) is
     void $ set (alarm dl `composeM` alarms dl) Map.empty
 
--------------
--- Landing --
--------------
+--------------------
+-- Memory Landing --
+--------------------
 
 data Network = Network { _n :: Gid
                        , _geofences :: Map Gid Geofence
@@ -155,6 +160,106 @@ stateDL = GeoDL {
   , wentOff = \ra -> StateT (\s -> Identity (runReaderT ra (
       Prelude.filter (\((_, _), t) -> t <= _current s) $ Map.toList $ _alarms s),
       s))
+  , region = fromLn $ Lens (\s -> (_region s, \a -> s { _region = a }))
+  , inside = fromLn $ Lens (\s -> (_in s, \a -> s { _in = a }))
+  , initG = \r -> Geofence r Set.empty }
+
+------------------
+-- HDBC Landing --
+------------------
+
+loadScheme :: IO ()
+loadScheme = do
+  conn <- connectSqlite3 "geofences.db"
+  run conn "CREATE table Counter (n INTEGER NOT NULL)" []
+  run conn "CREATE table Current (time INTEGER NOT NULL)" []
+  run conn "CREATE table Region (gid INTEGER NOT NULL, radius INTEGER NOT NULL, x INTEGER NOT NULL, y INTEGER NOT NULL)" []
+  run conn "CREATE table Inside (gid INTEGER NOT NULL, did INTEGER NOT NULL)" []
+  run conn "CREATE table Alarm (gid INTEGER NOT NULL, did INTEGER NOT NULL, time INTEGER NOT NULL)" []
+  run conn "INSERT INTO Counter VALUES (0)" []
+  run conn "INSERT INTO Current VALUES (0)" []
+  commit conn
+  disconnect conn
+
+newtype IOAlarms a = IOAlarms { runIOAlarms :: IO a }
+  deriving (Functor, Applicative, Monad)
+
+instance MonadState Alarms IOAlarms where
+  get = do
+    conn <- IOAlarms $ connectSqlite3 "geofences.db"
+    r <- IOAlarms $ quickQuery' conn "SELECT * from Current" []
+    let current = fromSql (head $ head r)
+    r <- IOAlarms $ quickQuery' conn "SELECT * from Alarm" []
+    let alarms = Map.fromList $ fmap (\[g, d, t] -> ((fromSql g, fromSql d), fromSql t)) r
+    IOAlarms $ commit conn >> disconnect conn
+    return $ Alarms alarms current
+  put a = do
+    conn <- IOAlarms $ connectSqlite3 "geofences.db"
+    r <- IOAlarms $ run conn "UPDATE Current SET time = ?" [toSql (_current a)]
+    IOAlarms $ run conn "DELETE * from Current" []
+    stmt <- IOAlarms $ prepare conn "INSERT INTO Alarms VALUES (?, ?, ?)"
+    IOAlarms $ executeMany stmt (fmap (\((g, d), t) -> [toSql g, toSql d, toSql t]) (Map.toList $ _alarms a))
+    IOAlarms $ commit conn >> disconnect conn
+
+newtype IOGeofence g = IOGeofence { runIOGeofence :: Gid -> IO g }
+
+class IMonadState i a m where
+  iget :: i -> m a
+  iput :: i -> a -> m ()
+
+hdbcDL :: GeoDL IO (State Geofence) (State (Maybe Geofence)) IOAlarms Geofence Alarms
+hdbcDL = GeoDL {
+    geofence = At' $ \gid -> \sa -> do
+      conn <- connectSqlite3 "geofences.db"
+      r <- quickQuery' conn "SELECT * from Region where gid=?" [toSql gid]
+      let regs :: Maybe (Gid, Region)
+          regs = fmap (\[g, r, x, y] -> (fromSql g, (fromSql r, (fromSql x, fromSql y)))) r
+      disconnect conn
+      undefined
+  , geofences = \f -> do
+      conn <- connectSqlite3 "geofences.db"
+      r <- quickQuery' conn "SELECT * from Region" []
+      let regs :: [(Gid, Region)]
+          regs = fmap (\[g, r, x, y] -> (fromSql g, (fromSql r, (fromSql x, fromSql y)))) r
+      xs <- traverse (\(g, reg) -> do
+        r <- quickQuery' conn "SELECT did from Inside where gid=?" [toSql g]
+        let (x, g2) = runState (f g) (Geofence reg (Set.fromList $ fmap (\[d] -> fromSql d) r))
+        run conn "DELETE * from Inside where gid=?" [toSql g]
+        stmt <- prepare conn "INSERT INTO Inside VALUES (?, ?)"
+        executeMany stmt (fmap (\d -> [toSql g, toSql d]) (Set.toList (_in g2)))
+        run conn "UPDATE Region SET radius=?, x=?, y=? where gid=?" [toSql (fst $ _region g2), toSql (fst $ snd $ _region g2), toSql (snd $ snd $ _region g2)]
+        return x) regs
+      disconnect conn
+      return xs
+  -- XXX: this natural transformation is almost `id`. This just happens because
+  -- focus program is self-sufficient.
+  , alarm = fmap Identity . runIOAlarms
+  , cnt = \sa -> do
+      conn <- connectSqlite3 "geofences.db"
+      r <- quickQuery' conn "SELECT * from Counter" []
+      let (a, s) = runState sa (fromSql (head $ head r))
+      run conn "UPDATE Counter SET n=?" [toSql s]
+      disconnect conn
+      return $ Identity a
+  , current = \sa -> do
+      a <- get
+      let (x, cu) = runState sa (_current a)
+      put $ a { _current = cu }
+      return (Identity x)
+  , alarms = \sa -> do
+      a <- get
+      let (x, as) = runState sa (_alarms a)
+      put $ a { _alarms = as }
+      return (Identity x)
+  , wentOff = \ra -> do
+      conn <- IOAlarms $ connectSqlite3 "geofences.db"
+      r <- IOAlarms $ quickQuery' conn "SELECT * from Counter" []
+      let curr :: Int; curr = fromSql (head $ head r)
+      r <- IOAlarms $ quickQuery' conn "SELECT * from Alarm where time < ?" [toSql curr]
+      let alarms :: [((Gid, Did), Time)]; alarms = fmap (\[g, d, t] -> ((fromSql g, fromSql d), fromSql t)) r
+      IOAlarms $ commit conn >> disconnect conn >> return (runReaderT ra alarms)
+  -- XXX: don't know hot to turn this into a database "program", since I need
+  -- the gid to determine which particular geofence I am updating.
   , region = fromLn $ Lens (\s -> (_region s, \a -> s { _region = a }))
   , inside = fromLn $ Lens (\s -> (_in s, \a -> s { _in = a }))
   , initG = \r -> Geofence r Set.empty }
