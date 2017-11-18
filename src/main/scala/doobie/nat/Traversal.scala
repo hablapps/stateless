ac093383230ef270a895cea771cfa38c71e35308
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2017-11-18 01:14:22 +0100

    simplify

--- src/main/scala/doobie/nat/Traversal.scala
+++ src/main/scala/doobie/nat/Traversal.scala
@@ -12,12 +12,10 @@ object TraversalDoobie{
   def fromStateT[K1,K2](dtrav: DoobieTraversal[K2,K1]) =
     TraversalAlg[StateT[ConnectionIO,K1,?],StateT[ConnectionIO,K2,?],K2]{
       new (StateT[ConnectionIO,K2,?] ~> λ[T=>StateT[ConnectionIO,K1,List[T]]]){
-        def apply[T](q: StateT[ConnectionIO,K2,T]): StateT[ConnectionIO,K1,List[T]] =
+        def apply[T](q: StateT[ConnectionIO,K2,T]) =
           StateT[ConnectionIO,K1,List[T]]{
             k: K1 => dtrav.getList(k) >>= { 
-              _.traverseU{
-                q(_).map{ case (_,out) => List(out) }
-              }.map(results => (k,results.flatten))
+              _.traverseU{q(_).map(_._2)}.map((k,_))
             }
           }
       }
