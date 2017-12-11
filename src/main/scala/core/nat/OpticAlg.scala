65aca89b62652cafcddd4ac9fc8df51f3c21fdf0
Jesús López-González <jesus.lopez@hablapps.com>
2017-12-11 10:20:55 +0100

    Removes unused optic algebra defs

--- src/main/scala/core/nat/OpticAlg.scala
+++ src/main/scala/core/nat/OpticAlg.scala
@@ -15,23 +15,3 @@ trait OpticAlg[P[_], A, Ev[M[_], _] <: Monad[M], F[_]] extends Monad[P] {
 
   val hom: Q ~> λ[x => P[F[x]]]
 }
-
-trait MROpticAlg[P[_], A, F[_]] extends OpticAlg[P, A, MonadReader, F] {
-
-  def view: P[F[A]] = hom(ev.ask)
-
-  def fold[M: Monoid](f: A => M)(implicit ev: Foldable[F]): P[M] =
-    map(view)(_.foldMap(f))
-}
-
-trait MSOpticAlg[P[_], A, F[_]] extends OpticAlg[P, A, MonadState, F] {
-
-  def view: P[F[A]] = hom(ev.get)
-
-  def set(a: A): P[F[Unit]] = hom(ev.put(a))
-
-  def modi(f: A => A): P[F[Unit]] = hom(ev.modify(f))
-
-  def fold[M: Monoid](f: A => M)(implicit ev: Foldable[F]): P[M] =
-    map(view)(_.foldMap(f))
-}
